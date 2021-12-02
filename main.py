c8d87cd43324636e02658fbe7b40c314543dda15
adnaneaabbar <adnaneaabbar@gmail.com>
2021-12-03 00:47:39 +0100

    fit, evaluate

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from keras.datasets import imdb
 from keras.layers import Dense, Embedding, LSTM
 from keras.models import Sequential
 from keras_preprocessing import sequence
+import matplotlib.pyplot as plt
 
 max_features = 20000
 maxlen = 80
@@ -9,9 +10,48 @@ maxlen = 80
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 
 model = Sequential()
 model.add(Embedding(max_features, 128))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
+
+# binary classification
+model.compile(loss='binary_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+history = model.fit(x_train,
+                    y_train,
+                    batch_size=32,
+                    epochs=15,
+                    validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test,
+                       y_test,
+                       batch_size=32)
+
+print('Test loss: ', score[0])
+print('Test accuracy: ', score[1])
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+filename = 'models/model.sav'
+model.save(filename)
