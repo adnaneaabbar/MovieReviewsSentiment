f3e278d685f4f3a17477ee24b6a715d55df9ffd1
adnaneaabbar <adnaneaabbar@gmail.com>
2021-12-03 00:41:30 +0100

    padding

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ max_features = 20000
 maxlen = 80
 
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+
+model = Sequential()
+model.add(Embedding(max_features, 128))
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
